08470a09590e6f9a9c732aff22f2edca02ea1f3a
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2018-10-20 18:01:54 -0600

    refactor: added color coding and removing unnecessary functions

--- 1-fundamentals-of-testing/testing-framework.js
+++ 1-fundamentals-of-testing/testing-framework.js
@@ -1,25 +1,24 @@
 const { sum, subtract } = require('./math');
-
-function sumTest() {
+const RED = '\x1b[32m';
+const GREEN = '\x1b[31m';
+test('sum adds numbers', () => {
   const result = sum(3, 7);
   const expected = 10;
   expect(result).toBe(expected);
-}
-test('sum adds numbers', sumTest);
+});
 
-function subtractTest() {
+test('subtract subtracts numbers', () => {
   const result = subtract(7, 3);
   const expected = 4;
   expect(result).toBe(expected);
-}
-test('subtract subtracts numbers', subtractTest);
+});
 
 function test(title, callback) {
   try {
     callback();
-    console.log(`✔ ${title}`);
+    console.log(RED, `✔ ${title}`);
   } catch (error) {
-    console.error(`✘ ${title}`);
+    console.error(GREEN, `✘ ${title}`);
     console.error(error);
   }
 }
